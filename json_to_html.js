d04897a76ce411c3a48fb8352f7cac8da1bb3ac3
trabant1100 <komelanczuk@live.com>
2024-07-24 21:50:53 +0200

    Nodejs version

---
+++ json_to_html.js
@@ -0,0 +1,52 @@
+const fs = require('node:fs/promises');
+const format = require('date-format');
+
+
+(async function main() {
+	const config = JSON.parse(await fs.readFile('config.json'));
+	const listingDir = config.listing.dir;
+	const today = format.asString('dd.MM.yyyy', new Date());
+	const auctionsDir = listingDir + '/' + today;
+	const imagesDir = 'images';
+
+	for (const filename of await fs.readdir(auctionsDir)) {
+		const fullFilename = `${auctionsDir}/${filename}`;
+		const stat = await fs.stat(fullFilename);
+		if (stat.isFile() && filename.endsWith('.json')) {
+			console.log(`Transforming ${filename}`);
+			const html = await createHtml(imagesDir, fullFilename);
+			const htmlFullFilename = fullFilename.replace('.json', '.html');
+			await fs.writeFile(htmlFullFilename, html);
+		}
+	}
+})();
+
+async function createHtml(imagesDir, fullFilename) {
+	const auction = JSON.parse(await fs.readFile(fullFilename));
+
+	let imgHtml = '';
+	for (let i = 1; i <= auction.imgUrls.length; i++) {
+		const imgUrl = `${auction.id}_${i}.webp`;
+		imgHtml += `<img src="${imagesDir}/${imgUrl}">`;
+	}
+
+	let html = `
+		<!doctype html>
+		<html lang=pl>
+		<head>
+			<meta charset=utf-8>
+		<title>${auction.title}</title>
+		</head>
+		<body>
+			<a href="${auction.url}"><h1>${auction.title}</h1></a>
+			<h2>${auction.description}</h2>
+			<h3>${auction.price} PLN</h3>
+			<h3>${auction.location}</h3>
+			<h3>${auction.mileage}</h3>
+			${imgHtml}
+		</body>
+		</html>
+	`;
+
+	return html;
+}
--- otomoto.js
+++ otomoto.js
@@ -1,22 +1,26 @@
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const format = require('date-format');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const config = JSON.parse(fs.readFileSync('config.json'));
-const listingDir = config.listing.dir;
-const listingUrl = config.listing.url;
-const today = format.asString('dd.MM.yyyy', new Date());
-const auctionsDir = listingDir + '/' + today;
-
-fs.mkdirSync(auctionsDir, { recursive: true });
-
 (async function main() {
+	const config = JSON.parse(await fs.readFile('config.json'));
+	const listingDir = config.listing.dir;
+	const listingUrl = config.listing.url;
+	const today = format.asString('dd.MM.yyyy', new Date());
+	const auctionsDir = listingDir + '/' + today;
+	const imagesDir = auctionsDir + '/images';
+
+	await fs.mkdir(auctionsDir, { recursive: true });
+	await fs.mkdir(imagesDir, { recursive: true });
+
+	console.log('Getting list of auctions');
 	const { data } = await axios.get(listingUrl);
 	const $ = cheerio.load(data);
 	const articles = $('article').toArray();
 	const auctions = [];
 
+	let auctionIdx = 0;
 	for (const el of articles) {
 		const jqSection = $(el).children('section');
 		const jqDivs = jqSection.children('div');
@@ -27,13 +31,21 @@ fs.mkdirSync(auctionsDir, { recursive: true });
 		const location = jqDivs.eq(2).children('dl').eq(1).children('dd').eq(0).children('p').text();
 
 		if (thumbnailUrl && url) {
+			console.log(`Getting details of auction #${auctionIdx + 1}`);
 			const details = await getAuctionDetails(url);
 			const auction = { thumbnailUrl, url, mileage, location, ...details };
-			console.log(auction);
-
-			break;
+			auctions.push(auction);
+			auctionIdx++;
 		}
 
+
+	}
+
+	for (const [idx, auction] of auctions.entries()) {
+		const auctionJson = JSON.stringify(auction, null, 2);
+		await fs.writeFile(`${auctionsDir}/${auction.id}.json`, auctionJson);
+		console.log(`Saving images auction ${idx + 1}/${auctions.length}`);
+		await saveImagesFromAuction(imagesDir, auction);
 	}
 })();
 
@@ -58,7 +70,24 @@ async function getAuctionDetails(url) {
 			.filter(url => srcImgRegex.test(url)) // image;s=148x110
 			.map(url => url.replace(srcImgRegex, 'image'));
 
-		resolve({ title, description, price, imgUrls });
+		const jqSectionDiv = jqMainDiv.children('section').eq(1).children('div').eq(0);
+		const date = jqSectionDiv.children('div').eq(0).children('p').text();
+		const id = jqSectionDiv.children('div').eq(1).children('p').text().replace(/^ID\s*:\s*/, '');
+
+		resolve({ title, description, price, date, id, imgUrls });
 	});
 }
 
+function saveImagesFromAuction(dir, auction) {
+	const { id, imgUrls } = auction;
+	const results = [];
+	for (const [idx, imgUrl] of imgUrls.entries()) {
+		results.push(saveImage(imgUrl, `${dir}/${id}_${idx + 1}.webp`));
+	}
+	return Promise.all(results);
+}
+
+async function saveImage(url, filename) {
+	const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+	return fs.writeFile(filename, data);
+}
